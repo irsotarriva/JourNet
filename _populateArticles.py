9ea4b608ec8836e6e7433c3a199eee25b59673f4
Isai Roberto Sotarriva Alvarez <isairobertosotarrivaalvarez@Isais-MacBook-Air.local>
2025-12-19 20:58:06 +0900

    Enhance article population and recommendation engine
    Features:
    - Implemented functions to insert authors and papers into the database.
    - Added functionality to resume processing from the last inserted paper.
    
    Refactorings:
    - Updated import statements to use models from the correct module.
    - Improved code structure for better readability and maintainability.

--- _populateArticles.py
+++ _populateArticles.py
@@ -2,23 +2,71 @@ import kagglehub
 import numpy as np
 import json
 from tqdm.auto import tqdm
+from connect_sql import get_supabase_client
+from supabase import Client
+
+def insert_author_into_db(supabase: Client, authorName: str) -> int:
+    """
+    @brief Inserts an author into the database if not already present.
+    @param supabase: Supabase client instance.
+    @param authorName: Name of the author to insert.
+    @return: ID of the inserted or existing author.
+    """
+    # Check if author already exists
+    existing_author = supabase.table("Authors").select("id").eq("name", authorName).execute()
+    if existing_author.data:
+        return existing_author.data[0]["id"]
+    # Insert new author
+    response = supabase.table("Authors").insert({"name": authorName}).execute()
+    return response.data[0]["id"]
+def insert_paper_into_db(supabase: Client, paperData: dict) -> int:
+    """
+    @brief Inserts a paper into the database.
+    @param supabase: Supabase client instance.
+    """
+    authors_str = paperData.pop("authors", "")
+    authors_list = [author.strip() for author in authors_str.split(",") if author.strip()]
+    author_ids = [insert_author_into_db(supabase, author) for author in authors_list]
+    submitter_id = insert_author_into_db(supabase, paperData.get("submitter", "Anonymous"))
+
+    paper_data_to_insert = {
+        "title": paperData.get("title"),
+        "submitter": submitter_id,
+        "authors": author_ids,
+        "journal_ref": paperData.get("journal-ref", ""),
+        "doi": paperData.get("doi", ""),
+        "report_number": paperData.get("report-no", ""),
+        "categories": paperData.get("categories", []),
+        "paper_license": paperData.get("license", ""),
+        "abstract": paperData.get("abstract", ""),
+        "updated_date": paperData.get("updated_date"),
+        "comments": paperData.get("comments", ""),
+    }
+    response = supabase.table("Papers").insert(paper_data_to_insert).execute()
+    return response.data[0]["id"]
 
 def main():
     # Download latest version
     print("Downloading latest version of arXiv dataset...")
-    path = kagglehub.dataset_download("Cornell-University/arxiv")
-    print("Path to dataset files:", path)
+    kaggleDSPath = kagglehub.dataset_download("Cornell-University/arxiv")
+    print("Path to dataset files:", kaggleDSPath)
     fields_to_ignore = {"id", "versions", "authors_parsed"}
+    supabase = get_supabase_client()
+    # To enable resuming from last processed line check supabase for last inserted paper index
+    last_id = supabase.table("Papers").select("id").order("id", desc=True).limit(1).execute()
+    last_id = int(last_id.data[0]["id"]) if last_id.data else 0
     # Count total lines (optional but gives accurate progress)
     with open(kaggleDSPath + "/arxiv-metadata-oai-snapshot.json", "r") as f:
         totalLines = sum(1 for _ in f)
     with open(kaggleDSPath + "/arxiv-metadata-oai-snapshot.json", "r") as f:
-        for line in tqdm(f, total=totalLines):
+        # If last_id exists, skip to that line
+        for lineNumber, line in enumerate(tqdm(f, total=totalLines, desc="Processing arXiv dataset")):
+            if lineNumber < last_id:
+                continue
             data = json.loads(line)
             paperData = {k: v for k, v in data.items() if k not in fields_to_ignore}
-            authors_str = paperData.pop("authors", "")
-            authors_list = [author.strip() for author in authors_str.split(",") if author.strip()]
-            paperData["authors"] = authors_list
+            insert_paper_into_db(supabase, paperData)
+
 
 if __name__ == "__main__":
     main()
--- recommend.py
+++ recommend.py
@@ -1,10 +1,11 @@
 #import server
-import paper
+import models
 
 from pydantic import BaseModel
 import os
 import numpy as np
-from qdrant_client import QdrantClient, models
+from qdrant_client import QdrantClient
+from qdrant_client import models as qdrant_models
 from qdrant_client.http.models import Filter, FieldCondition, MatchValue
 from sentence_transformers import SentenceTransformer
 
@@ -14,13 +15,43 @@ from sentence_transformers import SentenceTransformer
 class RecommendationEngine:
     qdrant_client = QdrantClient(
         url="https://ab493be3-1f1f-4d1b-9f6d-150d5daca89d.sa-east-1-0.aws.cloud.qdrant.io:6333", 
-        api_key=os.getenv("QDRANT_API_KEY")
+        api_key=os.getenv("QDRANT_API_KEY"),
+        check_compatibility=False
     )
     collection_name = "arxiv_papers"
     model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
     def __init__(self):
         pass
-    def get_recommendation_for_paper(self, embedding: np.ndarray, top_k: int = 5) -> list[paper.Paper]:
+    def get_paper_by_id(self, paper_id: str) -> models.Paper:
+        result = self.qdrant_client.retrieve(
+            collection_name=self.collection_name,
+            ids=[paper_id],
+        )
+        print("Scroll result:", result)
+        """
+        if not result:
+            return None
+        str_authors = result.payload.get("authors", "")
+        author_list = []
+        if str_authors:
+            authors_split = str_authors.split(", ")
+            for author_name in authors_split:
+                author_list.append(models.Author(name=author_name))
+        return models.Paper(
+            uuid=result.id,
+            title=result.payload.get("title", ""),
+            abstract=result.payload.get("abstract", ""),
+            authors=author_list,
+            journal_ref=result.payload.get("journal_ref", ""),
+            doi=result.payload.get("doi", ""),
+            report_number=result.payload.get("report_number", ""),
+            categories=result.payload.get("categories", "").split(", "),
+            paper_license=result.payload.get("paper_license", ""),
+            comments=result.payload.get("comments", "")
+        )
+        """
+        return None
+    def get_recommendation_for_paper(self, embedding: np.ndarray, top_k: int = 5) -> list[models.Paper]:
         search_results = self.qdrant_client.search(
             collection_name=self.collection_name,
             query_vector=embedding.tolist(),
@@ -33,8 +64,8 @@ class RecommendationEngine:
             if str_authors:
                 authors_split = str_authors.split(", ")
                 for author_name in authors_split:
-                    author_list.append(paper.Author(name=author_name))
-            recommendations.append(paper.Paper(
+                    author_list.append(models.Author(name=author_name))
+            recommendations.append(models.Paper(
                 uuid=result.id,
                 title=result.payload.get("title", ""),
                 abstract=result.payload.get("abstract", ""),
@@ -47,18 +78,18 @@ class RecommendationEngine:
                 comments=result.payload.get("comments", "")
             ))
         return recommendations
-    def get_recommendation(self, positive_embeddings: list[np.ndarray] = [], negative_embeddings: list[np.ndarray] = [], top_k: int = 5, filter: dict = None) -> list[paper.Paper]:
-        filter = models.Filter(
+    def get_recommendation(self, positive_embeddings: list[np.ndarray] = [], negative_embeddings: list[np.ndarray] = [], top_k: int = 5, filter: dict = None) -> list[models.Paper]:
+        filter = qdrant_models.Filter(
             must = [
-                models.FieldCondition(
+                qdrant_models.FieldCondition(
                     key=filed_name,
-                    match=models.MatchValue(value=field_value)
+                    match=qdrant_models.MatchValue(value=field_value)
                 ) for filed_name, field_value in filter.items()
             ]
         ) if filter else None
-        recomment_query = models.QueryRequest(
-            query = models.RecommendQuery(
-                recommend = models.RecommendInput(
+        recomment_query = qdrant_models.QueryRequest(
+            query = qdrant_models.RecommendQuery(
+                recommend = qdrant_models.RecommendInput(
                     positive = [emb.tolist() for emb in positive_embeddings],
                     negative = [emb.tolist() for emb in negative_embeddings],
                     strategy = "best_score"
@@ -78,8 +109,8 @@ class RecommendationEngine:
             if str_authors:
                 authors_split = str_authors.split(", ")
                 for author_name in authors_split:
-                    author_list.append(paper.Author(name=author_name))
-            recommendations.append(paper.Paper(
+                    author_list.append(models.Author(name=author_name))
+            recommendations.append(models.Paper(
                 uuid=result.id,
                 title=result.payload.get("title", ""),
                 abstract=result.payload.get("abstract", ""),
@@ -97,6 +128,7 @@ class RecommendationEngine:
 
 if __name__ == "__main__":
     engine = RecommendationEngine()
+    """
     # Example usage
     sample_positive_text1 = "Deep learning techniques for natural language processing"
     sample_positive_text2 = "Advancements in computer vision using convolutional neural networks"
@@ -106,6 +138,7 @@ if __name__ == "__main__":
     pos_emb2 = engine.embed_text(sample_positive_text2)
     neg_emb = engine.embed_text(sample_negative_text)
     print("Fetching recommendations")
+
     recommendations = engine.get_recommendation(
         positive_embeddings=[pos_emb1, pos_emb2],
         negative_embeddings=[neg_emb],
@@ -114,3 +147,8 @@ if __name__ == "__main__":
     )
     for paper_rec in recommendations:
         print(f"Paper ID: {paper_rec.uuid}, Title: {paper_rec.title}")
+    """
+    paper_id = "14"
+    paper_rec = engine.get_paper_by_id(paper_id)
+    if paper_rec:
+        print(f"Paper ID: {paper_rec.uuid}, Title: {paper_rec.title}", f"Authors: {[author.name for author in paper_rec.authors]}")
